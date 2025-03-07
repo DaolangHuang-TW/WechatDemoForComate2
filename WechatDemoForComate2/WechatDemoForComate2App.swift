310234b401ebce88e63e8dbc85aff865c59bd8e6
Daolang Huang <daolang.huang@thoughtworks.com>
2025-03-07 12:00:38 +0800

    Initial Commit

---
+++ WechatDemoForComate2/WechatDemoForComate2App.swift
@@ -0,0 +1,17 @@
+//
+//  WechatDemoForComate2App.swift
+//  WechatDemoForComate2
+//
+//  Created by Daolang Huang on 2025/3/7.
+//
+
+import SwiftUI
+
+@main
+struct WechatDemoForComate2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WechatDemoForComate2Tests/WechatDemoForComate2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WechatDemoForComate2Tests.swift
+//  WechatDemoForComate2Tests
+//
+//  Created by Daolang Huang on 2025/3/7.
+//
+
+import Testing
+@testable import WechatDemoForComate2
+
+struct WechatDemoForComate2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
